4231753f77a11809cb9d7977c37079b8dcdfaec9
Leonssizm <levani.kereselidze11@gmail.com>
2022-11-29 00:23:18 +0400

    feat: fetched blogs list-rendering

--- src/components/AllPosts.vue
+++ src/components/AllPosts.vue
@@ -3,14 +3,13 @@
     <h1 class="p-5 d-flex justify-content-center">Posts</h1>
     <div class='p-5 text-center'>
         <a href='create.html' class="btn btn-success">Add Post</a>
-        <hr>
     </div>
  </div>
- <div>
-    <span>{{posts.id}}</span>
-    <h3>{{posts.title}}</h3>
-    <p>{{posts.body}}</p>
-        <div class="btn-wrapper d-flex justify-content-between mt-5 ">
+ <div v-for="post in posts" :key="post.id" class="container border border-3 rounded mt-3">
+    <p class="mt-2">Post N{{post.id}}</p>
+    <h3 class="d-flex justify-content-center mt-3">{{post.title}}</h3>
+    <p class="mt-4">{{post.body}}</p>
+        <div class="btn-wrapper d-flex justify-content-around mb-3 mt-3">
             <a class="btn btn-primary">View</a>
             <button class="btn btn-danger">Delete</button>
         </div>
@@ -26,17 +25,12 @@ export default {
           posts:[],
         }
     },
-    methods: {
-        fetchAllPosts(){
+    mounted() {
             fetch('https://jsonplaceholder.typicode.com/posts')
             .then(response => response.json()).then((posts) =>{
-                posts.forEach((post) => {
-                   this.posts.push(post.id);
-                   this.posts.push(post.title);
-                   this.posts.push(post.body)
-                })
-            })
-        },
+                this.posts = posts
+        });
+            
     },
 }
 </script>
