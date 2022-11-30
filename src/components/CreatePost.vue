e69750b32eea7b73f0ea9964cea96077262f83e0
Leonssizm <levani.kereselidze11@gmail.com>
2022-11-30 20:40:03 +0400

    feat: add post & new-design:post submit popup modal

--- src/components/CreatePost.vue
+++ src/components/CreatePost.vue
@@ -7,10 +7,22 @@
         </div>
     </div>
 
+<div class="popup-wrapper invisible" id="popup">
+  <div class="popup">
+        <h1 class="header">Post has been recorded</h1> 
+    <div class="popup-btn-wrapper">
+        <router-link class="btn btn-primary" to="/posts">Main Page</router-link>
+        <button class="btn btn-danger" @click="togglePopup">Cancel</button>
+    </div> 
+  </div>
+</div>
+    
+
     <div class="d-flex justify-content-center p-5 ">
-        <button class="btn btn-primary mr-5"> Create </button>
-        <router-link to="/posts" class="btn btn-danger "> Cancel </router-link>
+        <button @click="submitForm()" class="btn btn-primary mr-5"> Create </button>
+        <router-link to="/posts" class="btn btn-danger"> Cancel </router-link>
     </div>
+
 </template>
 
 <!-- Under construction -->
@@ -22,6 +34,75 @@ export default {
             bodyInputValue: '',
         }
     },
+
+    methods: {
+        submitForm() {
+            const data = {
+                title: this.titleInputValue,
+                body: this.bodyInputValue
+            }
+            fetch('https://jsonplaceholder.typicode.com/posts', {
+                method: 'POST',
+                body: JSON.stringify(data)
+            })
+            .then((response) => {
+                response.json();
+                console.log(response)
+                if(response.ok){
+                    const popup = document.getElementById('popup');
+                    popup.classList.remove('invisible');
+                    popup.classList.add('visible');
+                }
+
+            })
+
+            
+        },
+        togglePopup() {
+            const popup = document.getElementById('popup');
+            popup.classList.remove('visible');
+            popup.classList.add('invisible');
+            
+        }
+    }
   
 }
 </script>
+
+<style>
+
+.popup{
+display: flex;
+justify-content: space-around;
+flex-direction: column;
+border:3px #2d545e solid;
+background-color:  #12343b;
+height:200px;
+width:500px;
+
+}
+.popup-btn-wrapper{
+    display: flex;
+    justify-content: space-evenly;
+}
+.header{
+    display:flex;
+    justify-content:center;
+    color:white;
+}
+.invisible{
+    display:none;
+}
+.visible{
+  display:flex;
+  justify-content:center;
+  align-items:center;
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  background: rgba(0, 0, 0, 0.8);
+}
+
+</style>
--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,11 +1,6 @@
 <template>
     <h1 class="p-5 d-flex justify-content-center">Hi, would you like to see our posts?</h1>
     <div class="d-flex justify-content-center">
-     <a class="btn btn-primary" href="/posts"> Load Posts </a>
+     <router-link class="btn btn-primary" to="/posts"> Load Posts </router-link>
     </div>
 </template>
-
-<script>
-
-
-</script>
